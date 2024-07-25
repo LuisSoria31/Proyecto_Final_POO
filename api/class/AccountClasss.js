fb974712a9f65f43ef91cb4c9c040e4eb0415dd1
LuisSoria31 <utm23090484@utma.edu.mx>
2024-07-25 13:54:57 -0600

    25/07 Creacion de clase de cuentas y tarjetas

---
+++ api/class/AccountClasss.js
@@ -0,0 +1,76 @@
+// Obtener cuenta
+// Obtener cuentas
+// Agregar balance
+// Restar Balance
+// Crear una cuenta
+import AccountModel from "../models/AccountModel.js";
+class ManagerAcount{
+    constructor(userID, accountNumber, accountType, balance){
+        this.userID = userID;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+
+    async getAccounts(){
+        try {
+            const accounts = await AccountModel.find();
+            return accounts;
+        } catch (error) {
+            throw new Error(`Error al obtener cuentas: ${error}`);
+        }
+    }
+
+    async getAccount(id){
+        try {
+            const account = await AccountModel.findById(id);
+            return account;
+        } catch (error) {
+            throw new Error(`Error al obtener cuenta: ${error}`);
+        }
+    }
+    
+    async addBalance(id, amount){
+        try {
+            this.balance += amount;
+            await AccountModel.findByIdAndUpdate(id,{
+                $set:{
+                    balance: this.balance 
+                }
+            })
+            return "Ok";
+        } catch (error) {
+            throw new Error(`Error al agregra monto: ${error}`);
+        }
+    }
+
+    async restBalance(id, amount){
+        try {
+            this.balance -= amount;
+            await AccountModel.findByIdAndUpdate(id,{
+                $set:{
+                    balance: this.balance 
+                }
+            })
+            return "Ok";
+        } catch (error) {
+            throw new Error(`Error al agregra monto: ${error}`);
+        }
+    }
+
+    async createAccount(){
+        try {
+            await AccountModel.create({
+                userId: this.userID,
+                accountNumber: this.accountNumber,
+                accountType: this.accountType,
+                balance: this.balance
+            });
+            return "Ok";
+        } catch (error) {
+            throw new Error(`Error al crear cuenta: ${error}`);
+        }
+    }
+}
+
+export default ManagerAcount;
